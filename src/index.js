6183abc72488feebca0751aab2eb1ab199d85f94
Andela Rwanda <andelarwanda@Andelas-MacBook-Pro.local>
2023-04-18 15:57:57 +0200

    Chore(Redux setup): Setting up Redux  - Install Redux  - Install react-redux [Finishes #184948240]

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from "react";
 import App from "./App";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-      <App/>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
