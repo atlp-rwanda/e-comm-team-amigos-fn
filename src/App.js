6069747786046fc483d7cd8aa1205e39a899562b
GasanaJr <gasanadj2003@gmail.com>
2023-04-18 13:13:44 +0200

    --amend

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import Logo from './assets/img/logo.png';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+import Routes from "./routes/index.jsx";
+
 const App = () => {
   return (
-    <div  className='container'>
-        <img src={Logo} alt='app logo'/>
-        <h1>Shop now and enjoy free shipping on all orders</h1>
-    </div>
-  )
-}
+    <Router>
+      <Routes></Routes>
+    </Router>
+  );
+};
 export default App;
