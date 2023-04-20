0c0e45e96fee2c7e380b04576d3eaa80f18c56de
Cyusa Kheven <cyusa.khevin100@gmail.com>
2023-04-20 11:32:00 +0200

    ch(deploy): setup automatic deployonbase branch and on PR

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,65 @@
-const path = require('path');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/',
-        assetModuleFilename: "assets/img/[hash][ext][query]"
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html',
-            favicon:"./src/assets/img/logo.png",
-        }),
-        new MiniCssExtractPlugin(),
-    ],
-    module: {
-        rules: [
-            { 
-                test: /\.css$/i,                
-                use: [                  {                    
-                loader: MiniCssExtractPlugin.loader,
-                options: { publicPath: "" },
-                },                  
-                "css-loader",                                                  
-                ],              
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: [['@babel/preset-env',{ targets: "defaults" }], ['@babel/preset-react',{runtime:"automatic"}]]
-                        }
-                }
-            },
-            {                
-                test: /\.(png|jpe?g|gif|svg)$/i,
-              
-                type: "asset",                                     
-              },
-        ]
-    },
-    devServer: {
-        hot: true,
-        port: 3000,
-        open:true,
-        historyApiFallback: true, 
-        historyApiFallback: {
-          disableDotRule: true
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+    assetModuleFilename: "assets/img/[hash][ext][query]",
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/assets/img/logo.png",
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./public/_redirects", to: "" }],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              ["@babel/preset-env", { targets: "defaults" }],
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+          },
         },
-    }
-}
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+
+        type: "asset",
+      },
+    ],
+  },
+  devServer: {
+    hot: true,
+    port: 3000,
+    open: true,
+    historyApiFallback: true,
+    historyApiFallback: {
+      disableDotRule: true,
+    },
+  },
+};
