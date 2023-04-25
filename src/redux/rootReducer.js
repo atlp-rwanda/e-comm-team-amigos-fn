2030e15e1b26ccdd55b1fc4a0a29232ac32526e0
Mystica <r.dukuzeyez@alustudent.com>
2023-04-25 19:09:19 +0200

    feat(login): Create login page
    - create a login page by email and password
    - validate the all input
    - integrate with backend
    - testing login page
    
    [Delivires #184948252]

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import signupFormReducer from './signup-form/signup-form.reducer';
 import verifyEmailReducer from './verify-email/verifyEmail.reducer';
+import authReducer from "./reducer/auth";
 
 const rootReducer = combineReducers({
 	signupForm: signupFormReducer,
 	verifyEmail: verifyEmailReducer,
+	authState: authReducer,
 });
 
 export default rootReducer;
